5f4d6526c3cfc474128831589881e4ec9f54fa68
Weiche <40076769+SenpaiRyu@users.noreply.github.com>
2024-04-16 12:40:47 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 
 # Сидка кошелька | формат "poopa", "zalupa"
 MNEMONICS = []
-# Апи ключ с toncenter
+# Апи ключ с toncenter https://toncenter.com, там ссылка на бота тг, берете ключ оттуда
 API_KEY = ""
