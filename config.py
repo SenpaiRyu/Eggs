2f7653585b5320b719861b870fe7333028b4139b
Weiche <40076769+SenpaiRyu@users.noreply.github.com>
2024-04-17 14:38:00 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,3 +3,7 @@
 MNEMONICS = []
 # Апи ключ с https://toncenter.com, там ссылка на бота тг, берете ключ оттуда
 API_KEY = ""
+# Адрес владельца нфт, в нашем случае это кошелек деплоера контракта продажи
+NFT_ADDRESS = "EQBmSy9SfRj44LZPi84NyvI4seJlZYSz33MM0rl78DnkCb2Z"
+# Адрес коллекции нфт
+NFT_COLLECTION_ADDRESS = "EQBFELwdddZzNFXhiZxE1i_4700KG7XmiDTLYzO7UJI-YuRk"
