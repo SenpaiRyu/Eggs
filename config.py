8368eae4019959928d2063dfcaa5b6ee2805daa7
Weiche <40076769+SenpaiRyu@users.noreply.github.com>
2024-04-19 18:44:21 +0300

    Update config.py

--- config.py
+++ config.py
@@ -3,9 +3,10 @@
 MNEMONICS = []
 # Апи ключ с https://toncenter.com, там ссылка на бота тг, берете ключ оттуда
 API_KEY = ""
-# Адрес владельца нфт, в нашем случае это кошелек деплоера контракта продажи
-NFT_ADDRESS = "EQBmSy9SfRj44LZPi84NyvI4seJlZYSz33MM0rl78DnkCb2Z"
-# Адрес коллекции нфт
+# EQAIFunALREOeQ99syMbO6sSzM_Fa1RsPD5TBoS0qVeKQ-AR - это офф деплоер гетгемса, все люди что выставляют на продажу через сайт взаимодействуют с этим контрактом.
+# Если фаундер проекта для выставления на продажу используют другой деплоер, то вам нужно поменять этот адрес.
+NFT_ADDRESS = "EQAIFunALREOeQ99syMbO6sSzM_Fa1RsPD5TBoS0qVeKQ-AR"
+# Адрес коллекции нфт, можно взять с ГетГемса
 NFT_COLLECTION_ADDRESS = "EQBFELwdddZzNFXhiZxE1i_4700KG7XmiDTLYzO7UJI-YuRk"
 # Цена покупки NFT в тонах
 NFT_PURCHASE_PRICE = 2
