2efe4da390ace724a12e2469427fa60f6d8e7da1
Weiche <40076769+SenpaiRyu@users.noreply.github.com>
2024-04-19 18:44:58 +0300

    Update config.py

--- config.py
+++ config.py
@@ -6,7 +6,7 @@ API_KEY = ""
 # EQAIFunALREOeQ99syMbO6sSzM_Fa1RsPD5TBoS0qVeKQ-AR - это офф деплоер гетгемса, все люди что выставляют на продажу через сайт взаимодействуют с этим контрактом.
 # Если фаундер проекта для выставления на продажу используют другой деплоер, то вам нужно поменять этот адрес.
 NFT_ADDRESS = "EQAIFunALREOeQ99syMbO6sSzM_Fa1RsPD5TBoS0qVeKQ-AR"
-# Адрес коллекции нфт, можно взять с ГетГемса
+# Адрес коллекции нфт, можно взять с ГетГемса, в примере адрес Кокошек owldaddy.ton
 NFT_COLLECTION_ADDRESS = "EQBFELwdddZzNFXhiZxE1i_4700KG7XmiDTLYzO7UJI-YuRk"
 # Цена покупки NFT в тонах
 NFT_PURCHASE_PRICE = 2
